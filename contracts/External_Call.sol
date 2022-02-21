15370aaa3cb9aa9b2d56cd3d14cad4a90f6b7121
yannis <yannis.ayouaz@gmail.com>
2022-02-21 13:20:38 +0100

    commit

--- contracts/External_Call.sol
+++ contracts/External_Call.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 // Mauvais
 contract Vulnerable {
     function withdraw(uint256 amount) external {
-        // This forwards 2300 gas, which may not be enough if the recipient
+        // This forwards 2300 gas, which may not be enough if the recipi ent
         // is a contract and gas costs change.
         payable(msg.sender).transfer(amount); 
     }
